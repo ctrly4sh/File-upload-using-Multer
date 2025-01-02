e18a14bdf2fc5616163eb5ede2d958c6a2262726
ctrly4sh <149458186+ctrly4sh@users.noreply.github.com>
2025-01-02 23:12:43 +0530

    some stuff

--- src/controller/fileController.ts
+++ src/controller/fileController.ts
@@ -2,29 +2,25 @@ import { Request, Response } from "express";
 
 export const getHealth = async (req: Request, res: Response): Promise<any> => {
   try {
-     return res.status(400).json({
+    return res.status(400).json({
       succes: true,
       message: "Server health okay ✅😃",
     });
   } catch (error) {
-     return res.json({
+    return res.json({
       success: false,
       message: error,
     });
   }
 };
 
-
 export const unZIP = (req: Request, res: Response): any => {
-
   if (!req.file) {
     return res.status(400).json({
       success: false,
       message: "No file uploaded",
     });
   }
-
-  
 
   return res.status(200).json({
     success: true,
@@ -35,5 +31,7 @@ export const unZIP = (req: Request, res: Response): any => {
       size: req.file.size,
     },
   });
+  
+  
+  return res.status(400).json({});
 };
-
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import fileRoutes from "./routes/fileRoutes";
 
 const app = express();
 
-app.use('/api' ,fileRoutes);
+app.use("/api", fileRoutes);
 
-app.listen(8900 , ()=>{
-    console.log("Server listening at localhost:8900")
+app.listen(8900, () => {
+  console.log("Server listening at localhost:8900");
 })
